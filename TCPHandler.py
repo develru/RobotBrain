a061ac3eeacf66bde944b226d49b63870a9a5970
Develru <develru@outlook.com>
2014-06-12 23:46:22 +0200

    TCP Handler added.

---
+++ TCPHandler.py
@@ -0,0 +1,64 @@
+__author__ = 'develru'
+
+
+from twisted.internet.protocol import Factory, Protocol
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import reactor
+from twisted.python import log
+import sys
+
+
+class DriveConnect(Protocol):
+
+    def __init__(self):
+        log.startLogging(sys.stdout)
+
+    def connectionMade(self):
+        print("Connect")
+        #self.sendLine('Connected')
+        self.transport.write("Connected")
+
+    def connectionLost(self, reason=connectionDone):
+        print("Connection lost")
+
+    def dataReceived(self, data):
+        print("Data received")
+        #data_str = data.decode(encoding='base64')
+        print(data)
+
+        if data == "FW":
+            print("Forward")
+        elif data == "BW":
+            print("Backward")
+        elif data == "RS":
+            print("Right")
+        elif data == "LS":
+            print("Left")
+
+
+class DriveFactory(Factory):
+
+    def buildProtocol(self, addr):
+        return DriveConnect()
+
+#import socketserver
+
+# class DriveTCPHandler(socketserver.StreamRequestHandler):
+#
+#     def handle(self):
+#         self.data = self.rfile.readline().strip()
+#         print("{} wrote".format(self.client_address[0]))
+#         print(self.data)
+#         self.wfile.write(self.data.upper())
+
+
+if __name__ == '__main__':
+    HOST, PORT = 'localhost', 9999
+
+    reactor.listenTCP(PORT, DriveFactory())
+    reactor.run()
+
+    #
+    # server = socketserver.TCPServer((HOST, PORT), DriveTCPHandler)
+    #
+    # server.serve_forever()
