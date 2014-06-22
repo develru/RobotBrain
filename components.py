da87c9ba2a6adfcfe65982773eb08b2f75254243
Develru <develru@outlook.com>
2014-06-22 15:17:50 +0200

    Drive module is implemented

--- components.py
+++ components.py
@@ -1,1 +1,53 @@
 __author__ = 'develru'
+
+from RPiPyLib import rpilib
+import time
+
+
+class DriveModule():
+    def __init__(self):
+        self.gpio17 = rpilib.GPIOrtx('17')
+        self.gpio17.export()
+        self.gpio17.setDirection('out')
+
+        self.gpio18 = rpilib.GPIOrtx('18')
+        self.gpio18.export()
+        self.gpio18.setDirection('out')
+
+        self.gpio22 = rpilib.GPIOrtx('22')
+        self.gpio22.export()
+        self.gpio22.setDirection('out')
+
+        self.gpio23 = rpilib.GPIOrtx('23')
+        self.gpio23.export()
+        self.gpio23.setDirection('out')
+
+    def offline(self):
+        self.gpio17.unexport()
+        self.gpio18.unexport()
+        self.gpio22.unexport()
+        self.gpio23.unexport()
+
+    def stop(self):
+        self.gpio17.writeValue('0')
+        self.gpio18.writeValue('0')
+        self.gpio22.writeValue('0')
+        self.gpio23.writeValue('0')
+
+    def drive_forward(self):
+        self.gpio18.writeValue('1')
+        self.gpio23.writeValue('1')
+
+    def drive_backward(self):
+        self.gpio17.writeValue('1')
+        self.gpio22.writeValue('1')
+
+    def steer_left(self):
+        self.gpio17.writeValue('1')
+        time.sleep(0.5)
+        self.gpio17.writeValue('0')
+
+    def steer_right(self):
+        self.gpio18.writeValue('1')
+        time.sleep(0.5)
+        self.gpio18.writeValue('0')
